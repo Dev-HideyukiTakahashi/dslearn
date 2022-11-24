3896188a859478439004950393df37ed29ee3688
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2022-11-24 05:38:42 -0300

    Course, Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,4 +14,10 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 
+INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://img.com', 'https://imggray.com');
+
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('1.0', '2020-11-20T03:00:07Z', '2021-11-20T03:00:07Z', 1);
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('2.0', '2020-12-20T03:00:07Z', '2021-12-20T03:00:07Z', 1);
+
+
 
